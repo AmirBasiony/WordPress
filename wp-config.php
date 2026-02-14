bcc11fe4a50f5c863478df093ae3ed72f5cf6d98
AmirBasiony <amirbasiony14@gmail.com>
2026-02-14 22:16:25 +0200

    Editing the wp-config  https forwarding

--- wp-config.php
+++ wp-config.php
@@ -91,6 +91,10 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) 
+    && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
 
 /* That's all, stop editing! Happy publishing. */
 
